541481a2567a6af50bb751e4c0b9668cc6d94f4c
roger chat <mattrogerpelosi@gmail.com>
2021-10-06 15:13:32 -0400

    updating connecting authors to books

--- my-app-frontend/src/components/NewBookForm.js
+++ my-app-frontend/src/components/NewBookForm.js
@@ -8,8 +8,8 @@ function NewBookForm({ newBookInput, setNewBook, handleSubmit, getAuthors }) {
     const animatedComponents = makeAnimated();
     const authorNames = [];
 
-    getAuthors.map(author => authorNames.push({value: author.name, label: author.name}))
-    console.log(authorNames)
+    getAuthors.map(author => authorNames.push({value: author.name, label: author.name, id: author.id}))
+//     console.log(getAuthors)
 
     const {title, image, isbn, desc, publisher, author} = newBookInput
 //     const [selectedTwo, setSelectedTwo] = useState([])
@@ -18,38 +18,39 @@ function NewBookForm({ newBookInput, setNewBook, handleSubmit, getAuthors }) {
         <div className='form'>
 
             <h2>Add A New Book!</h2>
+            <p>PLEASE NOTE: If you can't find your author, please create them first!</p>
 
             <form id='newBookForm' onSubmit={e => handleSubmit(e)}>
 
-                <input type='text'
+              <input type='text'
                        className='formText'
                        placeholder='Title'
                        name='title'
                        value={title}
                        onChange={e => setNewBook({...newBookInput, [e.target.name]: e.target.value})} />
 
-                <input type='text'
+              <input type='text'
                        className='formText'
                        placeholder='Cover Image'
                        name='image'
                        value={image}
                        onChange={e => setNewBook({...newBookInput, [e.target.name]: e.target.value})} />
 
-                <input type='text'
+              <input type='text'
                        className='formText'
                        placeholder='ISBN-10'
                        name='isbn'
                        value={isbn}
                        onChange={e => setNewBook({...newBookInput, [e.target.name]: e.target.value})} />
 
-                <input type='text'
+              <input type='text'
                        className='formText'
                        placeholder='Description'
                        name='desc'
                        value={desc}
                        onChange={e => setNewBook({...newBookInput, [e.target.name]: e.target.value})} />
 
-                <input type='text'
+              <input type='text'
                        className='formText'
                        placeholder='Publisher'
                        name='publisher'
@@ -59,10 +60,11 @@ function NewBookForm({ newBookInput, setNewBook, handleSubmit, getAuthors }) {
               <Select
                     closeMenuOnSelect={false}
                     components={animatedComponents}
+                     value={author}
+                    onChange={e => setNewBook(formerBook => ({...formerBook, author: e}))}
                     defaultValue={[authorNames[4]]}
                     isMulti
-                    options={authorNames}
-                    />
+                    options={authorNames} />
 
               <input type='submit'
                      value='Submit' />
